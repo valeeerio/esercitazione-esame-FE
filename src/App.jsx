1ece28aadfb84d2dd5e9a7ed7de317e230e9347e
valeeerio <valeriomortella03@gmail.com>
2024-04-12 17:40:15 +0200

    final

---
+++ src/App.jsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [nome, setNome] = useState();
+  const cambiaNome = (item) => {
+    setNome(item.target.value);
+  };
+
+  const [cognome, setCognome] = useState();
+  const cambiaCognome = (item) => {
+    setCognome(item.target.value);
+  };
+
+  const [email, setEmail] = useState();
+  const cambiaEmail = (item) => {
+    setEmail(item.target.value);
+  };
+
+  const [password, setPassword] = useState();
+  const cambiaPassword = (item) => {
+    setPassword(item.target.value);
+  };
+
+  const passaDati = async () => {
+    // console.log("nome: " + nome);
+    // console.log("conome: " + cognome);
+    // console.log("email: " + email);
+    // console.log("password: " + password);
+
+    const url = "http://localhost:8080/utente/registrazioni";
+
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        id: 1,
+        nome: nome,
+        cognome: cognome,
+        email: email,
+        password: password,
+      }),
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+    });
+  };
+
+  return (
+    <div className="registrazione">
+      REGISTRAZIONE
+      <div className="nome">
+        <textarea
+          placeholder="Inserisci il nome"
+          value={nome}
+          onChange={cambiaNome}
+        ></textarea>
+      </div>
+      <div className="cognome">
+        <textarea
+          placeholder="Inserisci il cognome"
+          value={cognome}
+          onChange={cambiaCognome}
+        ></textarea>
+      </div>
+      <div className="email">
+        <textarea
+          placeholder="mail"
+          value={email}
+          onChange={cambiaEmail}
+        ></textarea>
+      </div>
+      <div className="password">
+        <textarea
+          placeholder="password"
+          value={password}
+          onChange={cambiaPassword}
+        ></textarea>
+      </div>
+      <div className="registrati">
+        <button type="button" onClick={passaDati}>
+          registrati!
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
