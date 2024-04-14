53172b1b48212fddbe5e58a1333dc095a3c5e787
valeeerio <valeriomortella03@gmail.com>
2024-04-14 16:50:08 +0200

    modifica home page

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,3 +1,15 @@
 export default function HomePage() {
-  return <div className="home">Benvenuto nella pagina</div>;
+  return (
+    <div className="home">
+      Benvenuto nella pagina.
+      <div>
+        Per registrarti scrivi sulla barra di ricerca
+        http://localhost:3000/registrati
+      </div>
+      <div>
+        Per effettuare il login scrivi sulla barra di ricerca
+        http://localhost:3000/login
+      </div>
+    </div>
+  );
 }
